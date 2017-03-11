ea4214d9e0a70da4a03e6b7b6d8bcaf80991bfb7
Vriska <anirudhgan@gmail.com>
2017-03-11 15:11:55 +0530

    Add files via upload

---
+++ PonyScraper.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import re
+from math import floor
+import  time
+
+def Threads(FromYear,FromMonth) :
+    if FromMonth >=  12 :
+        FromYear += floor(FromMonth/12)
+        FromMonth = FromMonth % 12 + 1
+    link = r"https://www.equestriadaily.com/search/label/Drawfriend?updated-max="+str(FromYear)+"-"+str(FromMonth)+"-19T17:00:00-07:00&max-results=32&start=20&by-date=false"
+    r = requests.get(link)
+    soup = BeautifulSoup(str(r.text),"html.parser")
+    L = soup.find_all("a",string=re.compile('Drawfriend Stuff'))
+    L = [x.get('href') for x in L]
+    return L[1:]
+
+def ListGet(x):
+    try :
+        r = requests.get(str(x))
+        soup = BeautifulSoup(str(r.text),"html.parser")
+        PonyList = soup.find_all("a",string =re.compile('Source'))
+        PonyList= [x.get('href') for x in PonyList]
+        return PonyList
+    except :
+        print ('List Derp')
+
+Directory = input("Enter directory , default(hit enter) is ponypics folder in desktop : ")
+
+def GetImage(x) :
+    try:
+        r = requests.get(str(x))
+        soup = BeautifulSoup(r.text,"html.parser")
+        title = soup.find("a",class_="title").string
+        try :
+            author = soup.find("a",class_="u beta username").string
+        except :
+            author = soup.find("a",class_="u regular username").string
+        k = soup.find(class_=("dev-view-deviation"))
+        soup = BeautifulSoup(str(k),"html.parser")
+        L = soup.find_all("img")
+        URL = L[1].get("src")
+        r = requests.get(URL)
+        global Directory
+        Directory = Directory.replace("\\", "\\\\")
+        if not Directory:
+            if not os.path.exists(os.path.join(os.path.expanduser('~'), 'Desktop\\\ponypics\\')):
+                os.makedirs(os.path.join(os.path.expanduser('~'), 'Desktop\\\ponypics\\'))
+            FileName = os.path.join(os.path.expanduser('~'), 'Desktop\\\ponypics\\') + title + " by " + author +".png"
+        else :
+            if not os.path.exists(Directory):
+                os.makedirs(Directory)
+            FileName = Directory + "\\"  + title + " by " + author +".png"
+        try :
+            file = open(FileName, 'wb')
+        except :
+            print ("derpdoo")
+        file.write(r.content)
+        print (FileName)
+    except :
+        print ('SAAWWY I DEERPED')
+
+Month = int(input('Starting Month : '))
+Year = int(input('Starting year : '))
+Num = int(input ('Number of Months forward : '))
+Num = Month + Num
+
+StartThread = (input('Thread number to start from ? Defaults to first : '))
+if not StartThread:
+    StartThread = int(1)
+else :
+    StartThread = int(StartThread)
+StartPic = (input('Pic to start from? Defaults to first : '))
+if not StartPic :
+    StartPic = int(1)
+else :
+    StartPic = int(StartPic)
+
+ThreadCounter = 0
+for i in range(Month,Num+1):
+    for thread in Threads(Year,i)[StartThread-1:]:
+        for pic in ListGet(thread)[StartPic-1:]:
+            GetImage(pic)
+            print("month = " + str(i)+"   "+str(Threads(Year,i).index(thread)+1)+" of 32    " + str(ListGet(thread).index(pic)+1)+ " out of " + str(len(ListGet(thread))))
+            StartPic = 1
+            StartThread = 1
+            time.sleep(.3)
+
